62d0bbfe9d90da50f92f9d228bff8a16a3997d4c
ErgoCreate <31192003+ErgoCreate@users.noreply.github.com>
2025-07-24 15:45:08 -0600

    Create say_hi.py

---
+++ example_programs/say_hi.py
@@ -0,0 +1,29 @@
+import SimpleKivy.SimpleKivy as sk
+
+sk.auto_config()
+
+# All the stuff inside your window.
+lyt=[
+    [sk.Label('Input your name:')],
+    [sk.Input(k='i',size='y40')*sk.B('Say hi!',size='x100y40')],
+    [sk.T(k='msg')]
+]
+
+# Your main program must be inside a function with 2 arguments (app, event)
+# and should be added as the event_manager argument of the MyApp class.
+def evman(app,ev):
+    
+    # Detect the button released event
+    if ev=='Say hi!':
+        # Update the text of the "msg" widget
+        app('msg',text=f"Hi {app['i'].text}!")
+
+# Create the App
+app=sk.MyApp(
+    title="Say hi app",
+    layout=lyt,
+    event_manager=evman
+)
+
+# Run the App
+app.run()
