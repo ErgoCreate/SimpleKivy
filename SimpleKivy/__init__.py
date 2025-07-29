5fc5cc10054b1bae6803239447f9a5cef07c5e75
ErgoCreate <miguel_nunez95@hotmail.com>
2025-07-28 21:50:45 -0600

    Update __init__.py

--- SimpleKivy/__init__.py
+++ SimpleKivy/__init__.py
@@ -10,7 +10,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# along with this program. If not, see https://raw.githubusercontent.com/ErgoCreate/SimpleKivy/refs/heads/master/LICENSE
 #
 # For commercial licensing options, please contact the author at: [miguel_nunez95@hotmail.com].
 
